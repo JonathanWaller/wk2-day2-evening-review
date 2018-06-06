70bcd889ea5e86dbf4e89f214078c8e246338d35
bkchu <brandon.k.chung@gmail.com>
2018-06-05 19:03:43 -0500

    added comments

--- src/components/Characters/Character/Character.js
+++ src/components/Characters/Character/Character.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const Character = props => {
+  // I'm expecting to receieve props called obj, deleteElement, and id
+  // from the props that were passed in
   let { obj, deleteElement, id } = props;
   return (
-    <div onClick={() => deleteElement(id)}>
+    // the onClick on this outer div lets me delete the character as long as I click somewhere
+    //    within the div.
+    <div
+      style={{ border: '1px solid black', margin: '5px' }}
+      onClick={() => deleteElement(id)}
+    >
       <p>{obj.name}</p>
-      <br />
       <br />
     </div>
   );
--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -3,31 +3,56 @@ import axios from 'axios';
 import Character from './Character/Character';
 
 class Characters extends Component {
+  //****** the usual way!! ******
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     characters: ['Darth Vader', 'Cristian', 'Luke', 'Brandon'],
+  //     newCharacter: ''
+  //   };
+  // }
+
+  //****** the not-so-usual way!!! ******/
+  //props is still available here
   state = {
     characters: ['Darth Vader', 'Cristian', 'Luke', 'Brandon'],
     newCharacter: ''
   };
 
+  // arguably the most heavily used lifecycle hook
+  // used to make asynchronous calls.
+  // In this axios call, I'm grabbing an array of characters represented as objects, and then replacing the characters array on state.
+  // [ { name: 'Luke Skywalker', hair_color: 'blonde' }, { name, hair_color }, { same }, { same }, { same } ]
   componentDidMount() {
     axios.get('https://www.swapi.co/api/people').then(response => {
+      //if you check the console, you'll see the array of objects...
       console.log(response.data.results);
+
       this.setState({ characters: response.data.results });
     });
   }
 
+  // the following methods all use the 'this' keyword meaning I would normally have to bind,
+  // but I instead just turned them into arrow functions.
   onDeleteHandler = index => {
     const charactersCopy = this.state.characters.slice();
     charactersCopy.splice(index, 1);
     this.setState({ characters: charactersCopy });
   };
 
+  // same here
   onChangeHandler = e => {
     this.setState({ newCharacter: e.target.value });
   };
 
+  // same here
   onSubmitHandler = e => {
+    //prevents the page refresh on the 'onSubmit' event that usually happens after submitting.
     e.preventDefault();
 
+    // the ... (called the 'spread operator') copies the following array into a new array
+    // the { name: this.state.newCharacter } is the new object I want to add to the copied array.
+    // notice - the [] denotes the new array that I'm copying into.
     this.setState({
       characters: [...this.state.characters, { name: this.state.newCharacter }],
       newCharacter: ''
@@ -40,7 +65,9 @@ class Characters extends Component {
     const listOfCharacters = characters.map((character, index) => {
       return (
         <Character
+          //passing in props here.
           id={index}
+          //passed down the delete function so that the Character component can call it when it's clicked.
           deleteElement={this.onDeleteHandler}
           obj={character}
           key={index}
@@ -48,6 +75,8 @@ class Characters extends Component {
       );
     });
 
+    // putting the form with the onSubmitHandler along with button as the last thing in the form
+    //    allows me to hit the 'Enter' button to add a new item.
     return (
       <div>
         <form onSubmit={this.onSubmitHandler}>
